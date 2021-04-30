eaaffc1ef84b6992f1f3388bcdad2f885a173c43
ManaliSP <manalip054@gmail.com>
2021-04-30 17:26:29 +0530

    added college

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
       <header className="App-header">
         <h1>Welcome to GIT and GITHUB tutorial.</h1>
         <h2>Manali patil</h2>
+        <h3>GCEK</h3>
       </header>
     </div>
   );
