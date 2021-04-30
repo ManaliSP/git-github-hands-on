800d5bd85755724910a05a4e92cbc5c82af0b6b5
ManaliSP <manalip054@gmail.com>
2021-04-30 18:35:45 +0530

    aligned center

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to GIT and GITHUB tutorial.</h1>
-        <h2>Manali patil</h2>
-        <h3>GCEK</h3>
-        <h4>Roll No : 18143184</h4>
-        <h5>PIN Code : 415124</h5>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div align="center">
+                    <h1>Welcome to GIT and GITHUB tutorial.</h1>
+                    <h2>Manali patil</h2>
+                    <h3>GCEK</h3>
+                    <h4>Roll No : 18143184</h4>
+                    <h5>PIN Code : 415124</h5>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
