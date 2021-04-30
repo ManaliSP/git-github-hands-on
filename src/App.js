1b68f64783ebe20cbc61334a5d329d9b12049f36
ManaliSP <manalip054@gmail.com>
2021-04-30 17:25:52 +0530

    added name

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to GIT and GITHUB tutorial.</h1>
+        <h2>Manali patil</h2>
       </header>
     </div>
   );
